6cc90b44ea7ae35a2cf58e64514670125e5816d5
marco_polo <nol-95@mail.ru>
2021-06-23 16:15:22 +0300

    Дз после второго вебинара

--- src/hw2.java
+++ src/hw2.java
@@ -1,3 +1,43 @@
 
 public class hw2 {
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean number(int a, int b) {
+        int sum = a + b;
+        if (sum >= 10) {
+            if (sum <= 20) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        return false;
+    }
+
+    public static void number1(int a) {
+        if (a >= 0) {
+            System.out.println("Число положительное");
+        } else {
+            System.out.println("Число отрицытельное");
+        }
+    }
+
+
+    public static boolean number2(int a) {
+        if (a < 0) {
+            return true;
+        } else {
+        }
+        return false;
+    }
+
+    public static void number3(String home, int i){
+        for (int j = 0; j < i; j++) {
+            System.out.println("home");
+        }
+
+
+    }
 }
